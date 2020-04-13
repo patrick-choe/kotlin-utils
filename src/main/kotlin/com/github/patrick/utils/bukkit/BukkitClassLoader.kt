c54ed08048188cfad8e41bb6a38e269b3102e887
PatrickKR <mailpatrickkorea@gmail.com>
2020-04-13 15:31:46 +0900

    No more args support

--- src/main/kotlin/com/github/patrick/utils/bukkit/BukkitClassLoader.kt
+++ src/main/kotlin/com/github/patrick/utils/bukkit/BukkitClassLoader.kt
@@ -1,6 +1,5 @@
 package com.github.patrick.utils.bukkit
 
-import org.apache.commons.lang3.ClassUtils.toClass
 import org.apache.commons.lang3.reflect.ConstructorUtils.getMatchingAccessibleConstructor
 
 import java.util.ArrayList
@@ -8,7 +7,7 @@ import java.util.ArrayList
 @Suppress("unused")
 object BukkitClassLoader {
     @Throws(ClassNotFoundException::class, UnsupportedOperationException::class, UnsupportedClassVersionError::class, IllegalAccessError::class, InstantiationError::class)
-    fun <T> load(classType: Class<T>, prefix: String, version: String, vararg initialArgs: Any?): T {
+    fun <T> load(classType: Class<T>, prefix: String, version: String): T {
         val packageName = classType.`package`.name
         val className = "$prefix${classType.simpleName}"
         val candidates: MutableList<String> = ArrayList(2)
@@ -23,9 +22,8 @@ object BukkitClassLoader {
                 } catch (ignored: ClassNotFoundException) {}
             }
             (getMatchingAccessibleConstructor(
-                (nmsClass?: throw ClassNotFoundException("Not found class: $candidates")),
-                *toClass(initialArgs))?: throw UnsupportedOperationException("${classType.name} does not have constructor for [${toClass(initialArgs).joinToString()}]"
-            )).newInstance(initialArgs)
+                (nmsClass?: throw ClassNotFoundException("Not found class: $candidates")), null)?: throw UnsupportedOperationException("${classType.name} does not have constructor.]"
+            )).newInstance()
         } catch (e: ClassNotFoundException) { throw UnsupportedClassVersionError("${classType.name} does not support this version '$version'")
         } catch (e: IllegalAccessException) { throw IllegalAccessError("${classType.name} constructor is not visible")
         } catch (e: InstantiationException) { throw InstantiationError("${classType.name} is abstract class or an interface") }
